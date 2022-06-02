4b4249838d4ef34c58d168d7118ef8f70a5beb8d
Paul <paul21479@gmail.com>
2022-06-03 10:02:34 +1200

    Printing works but adding new data fails and pressing print will just print the entire thing.

--- main.py
+++ main.py
@@ -23,58 +23,41 @@ class Window:
         finally:
             customer.row += 1
             con.commit()
+            con.close()
             self.printf()
 # Create the second window with the customer list.
     def printf(self):
-        while True:
-            if len(self.nametxt.get()) == 0:
-                messagebox.showerror('No name', 'Please input a name')
-                break
-            try:
-                str(self.nametxt.get())
-                if len(self.receipttxt.get()) == 0:
-                    messagebox.showerror('No receipt', 'Input receipt')
-                    break
-                try:
-                    int(self.receipttxt.get())
-                    if len(self.itemtxt.get()) == 0:
-                        messagebox.showerror('Item', 'Insert an item')
-                    try: 
-                        str(self.itemtxt.get())
-                        if len(self.quantitytxt.get()) == 0:
-                            messagebox.showerror('Quantity', 'What is the quantity?')
-                            break
-                        try: 
-                            int(self.quantitytxt.get())
-                            if self.rootcount <= 1:
-                                self.root2 = Tk()
-                                self.root2.title('results')
-                                self.rootcount += 1
-                                Label(self.root2, text="Row").grid(column=0, row=0)
-                                Label(self.root2, text="Name").grid(column=1, row=0)
-                                Label(self.root2, text="Receipt").grid(column=2, row=0)
-                                Label(self.root2, text="Item").grid(column=3, row=0)
-                                Label(self.root2, text="Quantity").grid(column=4, row=0)
-                                self.root2.protocol("WM_DELETE_WINDOW", self.result_close)
-                                break
-                            # Display the inputs
-                            self.rname = Label(self.root2, text=customer.name).grid(row=self.rows, column=0, padx=20)
-                            self.rreceipt = Label(self.root2, text=customer.receipt).grid(row=self.rows, column=1, padx=20)
-                            self.ritem = Label(self.root2, text=customer.item).grid(row=self.rows, column=2, padx=20)
-                            self.rquantity = Label(self.root2, text=customer.quantity).grid(row=self.rows, column=3, padx=20)
-                            self.rows += 1
-                        except ValueError:
-                            messagebox.showerror('Quantity', 'Number only')
-                            break
-                    except ValueError:
-                        messagebox.showerror('Item', 'Letters only')
-                        break   
-                except ValueError:
-                    messagebox.showerror('Receipt', "Numbers only")
-                    break
-            except:
-                messagebox.showerror('Numbers?', "Input letters in name")
-                break
+        if self.rootcount <= 1:
+            self.root2 = Tk()
+            self.root2.title('results')
+            self.rootcount += 1
+            Label(self.root2, text="Row").grid(column=0, row=0)
+            Label(self.root2, text="Name").grid(column=1, row=0)
+            Label(self.root2, text="Receipt").grid(column=2, row=0)
+            Label(self.root2, text="Item").grid(column=3, row=0)
+            Label(self.root2, text="Quantity").grid(column=4, row=0)
+            self.root2.protocol("WM_DELETE_WINDOW", self.result_close)
+        #Database stuff
+        con = sqlite3.connect('save/save.db')
+        cursorObj = con.cursor()
+
+        cursorObj.execute("SELECT *, oid FROM customers")
+        records = cursorObj.fetchall()
+
+        for record in records:
+            rid = Label(self.root2, text=str(record[0]))
+            rid.grid(row=self.rows, column=0)
+            rname = Label(self.root2, text=str(record[1]))
+            rname.grid(row=self.rows, column=1)
+            rreceipt = Label(self.root2, text=str(record[2]))
+            rreceipt.grid(row=self.rows, column=2)
+            ritem = Label(self.root2, text=str(record[3]))
+            ritem.grid(row=self.rows, column=3)
+            rquantity = Label(self.root2, text=str(record[4]))
+            rquantity.grid(row=self.rows, column=4)
+            self.rows += 1
+        con.commit()
+        con.close()
     #when the second window is closed
     def result_close(self):
         self.rows = 1
